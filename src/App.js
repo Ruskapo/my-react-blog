55197ca7f0a503fb7337819281c3a13faad071fa
Alexandr Lebedev <lebedev-2018@bk.ru>
2025-11-05 23:09:34 +0500

    init: start react project base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useMemo } from "react";
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+import "./styles/App.css";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
+import PostFilter from "./components/PostFilter";
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "JavaScript", body: "Decription" },
+    { id: 2, title: "JavaScript 2", body: "Decription" },
+    { id: 3, title: "JavaScript 3", body: "Decription" },
+  ]);
+
+  const [filter, setFilter] = useState({ sort: "", query: "" });
+
+  const sortedPosts = useMemo(() => {
+    if (filter.sort) {
+      return [...posts].sort((a, b) =>
+        a[filter.sort].localeCompare(b[filter.sort])
+      );
+    }
+    return posts;
+  }, [filter.sort, posts]);
+
+  const sortedAndSearchedPost = useMemo(() => {
+    return sortedPosts.filter((post) =>
+      post.title.toLowerCase().includes(filter.query.toLowerCase())
+    );
+  }, [filter.query, sortedPosts]);
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+  };
+
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PostForm create={createPost}></PostForm>
+      <hr style={{ margin: "15px 8" }} />
+      <PostFilter filter={filter} setFilter={setFilter} />
+      {sortedAndSearchedPost.length !== 0 ? (
+        <PostList
+          remove={removePost}
+          posts={sortedAndSearchedPost}
+          title="Посты про JS"
+        />
+      ) : (
+        <h1 style={{ textAlign: "center" }}>Посты не были найдены!</h1>
+      )}
     </div>
   );
 }
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import MyButton from "./UI/button/MyButton";
+
+
+const PostItem = (props) => {
+  
+  return (
+    <div className="App">
+      <div className="post">
+        <div className="post__content">
+          <strong>{props.number}. {props.post.title}</strong>
+          <div>
+            {props.post.body}
+          </div>
+        </div>
+        <div className="post__bths">
+          <MyButton onClick={() => props.remove(props.post)}>
+            Удалить
+            </MyButton>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostItem;
